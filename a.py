1c353007ec2120945067b91286ae032f55be0ea0
Haseeb Ur Rehman <haseeb_ur_rehman829@yahoo.com>
2019-05-25 17:02:58 +0500

    assignment2.0

---
+++ a.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+a="PaKIsTan ZIndabad"
+print(a)
+
+
+# In[21]:
+
+
+a.capitalize()
+
+
+# In[22]:
+
+
+a.partition()
+
+
+# In[24]:
+
+
+a.center(0)
+
+
+# In[25]:
+
+
+a.expandtabs(2)
+
+
+# In[26]:
+
+
+help(print)
+
+
+# In[39]:
+
+
+print("qasim", "haseeb", sep="\n")
+
+
+# In[43]:
+
+
+print("Qasim", end=" ")
+print("Aslam", end=" ")
+print("Pakistani", end=" ")
+print(50, end=" ")
+
+
+# In[35]:
+
+
+print("Qasim", 'Aslam',"Pakistani", 50, sep="\n")
+
